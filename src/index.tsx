a4547f77671b7e2102911682459899888a6c69e6
chotaegyu <chotg5592@gmail.com>
2024-04-26 00:15:38 +0900

    FEAT: Add prettier, vitest, react-testing-library

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ function nl2br(input: string): (string | ReactElement)[];
 function nl2br<T>(input: T): T;
 
 function nl2br<T>(input: T | string): (string | ReactElement)[] | T {
-    if (typeof input === "string") {
-        const newlineRegex = /\r\n|\n|\r/g;
-        return input
-            .split(newlineRegex)
-            .flatMap((part, index, array) =>
-                index < array.length - 1 ? [part, <br key={index} />] : [part],
-            );
-    }
-    return input;
+  if (typeof input === "string") {
+    const newlineRegex = /\r\n|\n|\r/g;
+    return input
+      .split(newlineRegex)
+      .flatMap((part, index, array) =>
+        index < array.length - 1 ? [part, <br key={index} />] : [part],
+      );
+  }
+  return input;
 }
 
 export default nl2br;
